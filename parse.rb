38e5314a8be79f722a55d33e5719e67b4fe9d245
Fred Ollinger <follinge@gmail.com>
2014-06-30 08:16:28 -0700

    More details.

--- parse.rb
+++ parse.rb
@@ -5,6 +5,8 @@ require "rexml/document"
 file = File.new("cyclingunbound.housing.2014-06-30.xml")
 doc = REXML::Document.new file
 channel=doc.root.elements[1]
-
 #doc.root.each_element_with_text( 'wp:category_nicename' ) {|e| p e}
-channel.each_element() {|e| p e}
+#doc.root.elements.each("channel/item/wp:category") {|e| p e}
+channel.elements.each() {|e| 
+	p e.name
+}
