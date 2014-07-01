cd509ee425c97f23f7a8c1fa11ffce8654ee3a92
Fred Ollinger <follinge@gmail.com>
2014-06-30 20:16:33 -0700

    Get all the categories in an array almost done.

--- parse.rb
+++ parse.rb
@@ -7,6 +7,14 @@ doc = REXML::Document.new file
 channel=doc.root.elements[1]
 #doc.root.each_element_with_text( 'wp:category_nicename' ) {|e| p e}
 #doc.root.elements.each("channel/item/wp:category") {|e| p e}
-channel.elements.each() {|e| 
-	p e.name
+channel.each_element_with_text() {|e| 
+    #p "hi"
+    if "category" == e.name then
+	e.each_element_with_text() {|e| 
+    		if "category_nicename" == e.name then
+			p e.name
+			p e.text
+		end
+     }
+    end
 }
