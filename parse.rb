029a481e2c8a16204ade4a9beed04ea8b0bcb3a5
Fred Ollinger <follinge@gmail.com>
2014-07-04 21:31:38 -0700

    Now we extract text, title, and category.

--- parse.rb
+++ parse.rb
@@ -10,10 +10,26 @@ channel=doc.root.elements[1]
 #doc.root.elements.each("channel/item/wp:category") {|e| p e}
 arr = Array.new
 channel.each_element_with_text() {|e| 
-    p e.name
+    #p e.name
     e.each_element_with_text() {|e| 
 	att=e.attribute("nicename")
-	p att.class
+	if nil != att then
+		#p att.class
+		#p att.value
+	end
+	# p e.name
+	# p e.namespace
+
+	if  "http://purl.org/rss/1.0/modules/content/" == e.namespace and
+ 	     "encoded" == e.name	then
+		#p e.name
+		#p e.namespace
+		cdata = e.cdatas
+		#p cdata[0]
+	end
+ 	if "title" == e.name then
+		p e.text
+	end
     } # e.each_element_with_text() {|e| 
 } # channel.each_element_with_text() {|e| 
 
