7b01ab7579a16a8f3c9c15bee779b03399cc1a8b
Fred Ollinger <follinge@gmail.com>
2014-07-05 18:51:06 -0700

    Book is started.

--- parse.rb
+++ parse.rb
@@ -2,35 +2,43 @@
 
 require "rexml/document"
 
+def friendly_filename(filename)
+	filename.gsub(/[^\w\s_-]+/, '')
+        .gsub(/(^|\b\s)\s+($|\s?\b)/, '\\1\\2')
+	.gsub(/\s+/, '_')
+end
+
 #file = File.new("cyclingunbound.housing.2014-06-30.xml")
 file = File.new("cyclingunbound.wordpress.2014-06-30.xml")
 doc = REXML::Document.new file
 channel=doc.root.elements[1]
-#doc.root.each_element_with_text( 'wp:category_nicename' ) {|e| p e}
-#doc.root.elements.each("channel/item/wp:category") {|e| p e}
 arr = Array.new
+dir="cyclingunbound/uncategorized"
+cdata=""
+title=""
 channel.each_element_with_text() {|e| 
-    #p e.name
     e.each_element_with_text() {|e| 
 	att=e.attribute("nicename")
 	if nil != att then
-		#p att.class
-		#p att.value
+		dir="cyclingunbound/#{att}"
+		`mkdir -p #{dir}`
 	end
-	# p e.name
-	# p e.namespace
 
 	if  "http://purl.org/rss/1.0/modules/content/" == e.namespace and
  	     "encoded" == e.name	then
-		#p e.name
-		#p e.namespace
-		cdata = e.cdatas
-		#p cdata[0]
+		cdata = e.cdatas[0]
 	end
  	if "title" == e.name then
-		p e.text
+		title="#{dir}/"+friendly_filename(e.text)+".txt"
+	end
+	if "" != title and "" != cdata then
+		somefile = File.open(title, "w")
+		somefile.puts cdata 
+		somefile.close
+		title=""
+		cdata=""
+
 	end
     } # e.each_element_with_text() {|e| 
 } # channel.each_element_with_text() {|e| 
 
-#p arr
